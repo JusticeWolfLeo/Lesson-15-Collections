0b7fa5f07fb62290dbabf72a5f4c1c659ce822b8
JusticeWolfLeo <115044383+JusticeWolfLeo@users.noreply.github.com>
2023-03-15 11:12:08 +0300

    Add files via upload

--- MapsPractice.java
+++ MapsPractice.java
@@ -3,10 +3,9 @@ import java.util.Map;
 
 public class MapsPractice {
     public static void main(String[] args) {
-        String string = new String("Hello world");
-        String[] Split = string.split("");
-        Map<String, Integer> map = new HashMap<>();
-        for (var i : Split) {
+        char[] chr = "Hello world".toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        for (var i : chr) {
             if (map.containsKey(i)) {
                 map.replace(i, map.get(i) + 1);
             } else {
@@ -16,3 +15,16 @@ public class MapsPractice {
         System.out.println(map);
     }
 }
+/*
+    String string = new String("Hello world");
+    String[] Split = string.split("");
+    Map<String, Integer> map = new HashMap<>();
+        for (var i : Split) {
+                if (map.containsKey(i)) {
+                map.replace(i, map.get(i) + 1);
+                } else {
+                map.put(i, 1);
+                }
+                }
+                System.out.println(map);
+*/
